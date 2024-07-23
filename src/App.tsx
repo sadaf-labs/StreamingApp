a2e82bd22ffc43e306ce24124d99b20b6969c0a7
CaptSadaf <sadafadrees@gmail.com>
2024-07-23 22:38:17 +0500

    added error boundary component

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import * as serviceWorker from './serviceworker';
 import "./App.scss";
 import Home from './pages/Home';
+import { ErrorBoundary } from 'react-error-boundary';
+import ErrorFallBack from './components/error/error-boundary';
 
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <ErrorBoundary FallbackComponent={ErrorFallBack} >
+      <div className="App">
+        <Home />
+      </div>
+    </ErrorBoundary>
   );
 };
 
