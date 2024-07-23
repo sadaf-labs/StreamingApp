d961af8f69f3c93a2c5a5100cefe25f5559f049a
CaptSadaf <sadafadrees@gmail.com>
2024-07-23 22:43:26 +0500

    updated the service-worker path after adding github pages

--- src/serviceworker.ts
+++ src/serviceworker.ts
@@ -2,7 +2,7 @@ export function register() {
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', async () => {
       try {
-        const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+        const swUrl = `/service-worker.js`;
         await navigator.serviceWorker.register(swUrl);
       } catch (error) {
         console.error('Service Worker registration failed: ', error);
