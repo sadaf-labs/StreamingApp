8a03e7c15905104c1057ddd03a98eb12770b2083
CaptSadaf <sadafadrees@gmail.com>
2024-07-23 23:33:19 +0500

    github publish pages correct path

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -3,10 +3,10 @@ export const labels = {
   FAILED_TO_FETCH_ITEMS: 'Failed to fetch items'
 }
 
-const publicUrl = process.env.PUBLIC_URL;
-export const HOME_BANNER_URL = `${publicUrl}/home-banner.jpg`;
 
-export const COMPANY_LOGO_URL = `${publicUrl}/company-logo.png`;
+export const HOME_BANNER_URL = `/home-banner.jpg`;
+
+export const COMPANY_LOGO_URL = `/company-logo.png`;
 
 export const VIDEO_URLS = [
   "https://www.youtube.com/embed/gCNeDWCI0vo",
