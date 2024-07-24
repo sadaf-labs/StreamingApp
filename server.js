dd3abd58f84af63bcc588d340be09d3f414bf424
CaptSadaf <sadafadrees@gmail.com>
2024-07-24 11:25:09 +0500

    API fetch failing on mobile devices

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 import jsonServer from 'json-server';
+import cors from 'cors';
 
 const server = jsonServer.create();
 const router = jsonServer.router('./src/service/data/dummyData.json');
 const middlewares = jsonServer.defaults();
 
+server.use(cors());
 server.use(middlewares);
-
 server.use(router);
 
-server.listen(4000, () => {
-  console.log('JSON Server running on port 4000');
+const PORT = process.env.PORT || 4000;
+server.listen(PORT, () => {
+  console.log('JSON Server running on port', PORT);
 });
